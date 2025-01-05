6cebe05779cfa23fdaa5502a6b2597d696bd2bb5
qkrwlalss <128897708+qkrwlalss@users.noreply.github.com>
2025-01-05 15:16:53 +0900

    feat: 모달 컴포넌트

---
+++ src/components/Modal/styles.jsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const ModalContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.2);
+`;
+
+export const ModalContent = styled.div`
+  background: white;
+  border: 2px solid #0234A8;
+  padding: 36px 36px;
+  border-radius: 10px;
+  > p {
+    font-weight: 600;
+    display: flex;
+    justify-content: center;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`;
