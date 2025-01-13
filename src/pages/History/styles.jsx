169bd81335348b3f5a17329ad08feb4e5f21edcb
qkrwlalss <128897708+qkrwlalss@users.noreply.github.com>
2025-01-13 20:57:25 +0900

    feat: 내역 추가 페이지

---
+++ src/pages/History/styles.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 10vh;
+`;
+
+export const WrapperPC = styled.div`
+    width: 500px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 10vh;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 40px;
+  margin-top: 24px;
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  margin-top: 20px;
+`;
