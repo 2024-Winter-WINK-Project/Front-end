ddf2586879a2565c5ed00b7e8d8199433b4790db
qkrwlalss <128897708+qkrwlalss@users.noreply.github.com>
2025-01-16 01:16:05 +0900

    feat: 가계부 페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login/login.jsx";
 import MyPage from "./pages/MyPage/myPage.jsx";
+import Budget from "./pages/Budget/budget.jsx";
 import History from "./pages/History/history.jsx";
 
 let vh = window.innerHeight * 0.01
@@ -19,6 +20,7 @@ const App = () =>{
                 <Route path="/" element={<Home />}></Route>
                 <Route path="/login" element={<Login />}></Route>
                 <Route path="/mypage" element={<MyPage />}></Route>
+                <Route path="/budget" element={<Budget />}></Route>
                 <Route path="/history" element={<History />}></Route>
             </Routes>
         </BrowserRouter>
